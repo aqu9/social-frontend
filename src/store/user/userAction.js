443179c1500a38b532a2744de746829636aa8a11
aquibAhmad <aquibahamd.vb@ensuredit.com>
2023-05-11 13:46:09 +0530

    chnage baseurl

--- src/store/user/userAction.js
+++ src/store/user/userAction.js
@@ -1,9 +1,8 @@
-import axiosInstance from "../../helper/axiosInstance";
+import axiosInstance from '../../helper/axiosInstance';
 const uploadFile = (body) => {
   return async (dispatch, getState) => {
     try {
       const { data } = await axiosInstance({
-        baseURL: 'http://localhost:3000/',
         method: 'POST',
         url: `/uploadImage`,
         data: body,
@@ -20,7 +19,6 @@ const addNewPost = (body) => {
   return async (dispatch, getState) => {
     try {
       const { data } = await axiosInstance({
-        baseURL: 'http://localhost:3000/',
         method: 'POST',
         url: `/post`,
         data: body,
